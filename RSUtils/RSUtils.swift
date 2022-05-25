5496a25dfc9a1da868b27b7c3dfca18efce5861b
Nguyễn Hà <hanh.paraline@gmail.com>
2022-05-25 08:29:22 +0700

    add demo lib

---
+++ RSUtils/RSUtils.swift
@@ -0,0 +1,18 @@
+//
+//  RSUtils.swift
+//  RSUtils
+//
+//  Created by Nguyễn Hà on 25/05/2022.
+//
+
+import Foundation
+
+public final class RSUtils {
+
+    let name = "RSUtils"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+
+}
